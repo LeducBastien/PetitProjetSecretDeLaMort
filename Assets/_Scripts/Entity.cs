07b95dd6785b024bcb78c01b31a77def6f551333
ITALIANI Matteo <m.italiani@student.isartdigital.com>
2017-06-19 16:27:45 +0200

    control player done

--- Assets/_Scripts/Entity.cs
+++ Assets/_Scripts/Entity.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Entity : MonoBehaviour
 {
     [SerializeField] protected uint Life { get; set; }
+    [SerializeField] protected float Speed { get; set; }
+    [SerializeField] protected float Gravity { get; set; }
 
     protected virtual void Start()
     {
--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class Player : Entity
 {
-	// Use this for initialization
-	void Start () {
-		
+    private float jumpSpeed = 8.0f;
+    private Vector3 MoveDir = Vector3.zero;
+
+    // Use this for initialization
+    protected override void Start ()
+    {
+        Life = 10;
+        Speed = 10.0f;
+        Gravity = 9.8f;	
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    protected override void Update ()
     {
         CharacterController controller = GetComponent<CharacterController>();
 
-        if (controller.isGrounded)
+        if (controller)
         {
-           // if (controller.)
+            if (controller.isGrounded)
+            {
+                MoveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+                MoveDir = transform.TransformDirection(MoveDir);
+                MoveDir *= Speed;
+            }
+
+            MoveDir.y -= Gravity * Time.deltaTime;
+            controller.Move(MoveDir * Time.deltaTime);
         }
 	}
 }
