1f6e2142a060e097599b5fd8ac28b32e3a14078f
ITALIANI Matteo <m.italiani@student.isartdigital.com>
2017-06-19 16:06:03 +0200

    add mtkl and player script

--- Assets/_Scripts/Entity.cs
+++ Assets/_Scripts/Entity.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Entity : MonoBehaviour {
-
+public class Entity : MonoBehaviour
+{ 
 	// Use this for initialization
 	void Start () {
 		
